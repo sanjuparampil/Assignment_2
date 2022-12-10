00d4d6dd2cd20b6bf878043c2d9abd03775ae46f
sanjuparampil <95517660+sanjuparampil@users.noreply.github.com>
2022-12-10 00:47:56 +0000

    Update assignment__2.py
    Added codes for line graph.

--- assignment__2.py
+++ assignment__2.py
@@ -3,18 +3,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 rows = [0,4,9,14,21,41,82,110,120,234,245,252]
-#columns = [0,1,54,55,56,57,58,59]
 columns = [0,1,34,44,54,63]
-#49,50,51,52,53,
-#i = ['Africa Western and Central','United Arab Emirates','Australia','Bangladesh','China','United Kingdom','India','Japan','Saudi Arabia','United States']
 
 df_population = pd.read_csv("Population.csv",skiprows=lambda x: x not in rows)
 df_population = df_population. iloc[:, columns]
 df_population = df_population.set_index('Country Name')
 
-print(df_population)
+df_agriculture = pd.read_csv("Agriculture.csv",skiprows=lambda x: x not in rows)
+df_agriculture = df_agriculture. iloc[:, columns]
+df_agriculture = df_agriculture.drop(columns={"Country Name","Country Code"})
 
-#plt.title("Electricity consumption",size = 40)
 x = np.arange(len(df_population["Country Code"]))
 
 
@@ -62,7 +60,43 @@ def bar_no2(z):
     plt.xticks(x,df_no2['Country Code'], fontsize=22)#rotation=90)
     #plt.savefig('bar1.png', bbox_inches="tight", dpi = 300)
     plt.show()
+#Reading data to dataframe and selecting the required rows and columns
+
+
+#Transposing the dataframe
+df_agriculture_tr = df_agriculture.transpose()
+df_agriculture_tr=df_agriculture_tr.rename(columns={0:"AFW",1:"ARE",2:"AUS",3:"BGD",4:"CHN",5:"GBR",6:"IND",7:"JPN",8:"THA",9:"TUR",10:"USA"})
+
+print(df_agriculture_tr)
+#Plotting the line graph
+
+def line_agriculture(v):
+    """ This function plots line graph for years(x-axis)
+    vs Agriculture land area(y-axis).Visualization method used 
+    is LINE GRAPH"""
     
+    plt.figure(figsize=(35,20))
+    plt.plot(v.index, df_agriculture_tr['AFW'],'--',label = "AFW")
+    plt.plot(v.index, df_agriculture_tr['ARE'],'--',label = "ARE")
+    plt.plot(v.index, df_agriculture_tr['AUS'],'--',label = "AUS")
+    plt.plot(v.index, df_agriculture_tr['BGD'],'--',label = "BGD")
+    plt.plot(v.index, df_agriculture_tr['CHN'],'--',label = "CHN")
+    plt.plot(v.index, df_agriculture_tr['GBR'],'--',label = "GBR")
+    plt.plot(v.index, df_agriculture_tr['IND'],'--',label = "IND")
+    plt.plot(v.index, df_agriculture_tr['JPN'],'--',label = "JPN")
+    plt.plot(v.index, df_agriculture_tr['THA'],'--',label = "THA")
+    plt.plot(v.index, df_agriculture_tr['TUR'],'--',label = "TUR")
+    plt.plot(v.index, df_agriculture_tr['USA'],'--',label = "USA")
+    plt.xlabel("Years",fontsize = 30)
+    plt.legend(fontsize = 25)
+    plt.title('Agricultural land',size = 30)
+    #plt.xticks(x,df_agriculture_tr['Country Code'], fontsize=22)#rotation=90)
+    plt.xticks(fontsize=25)
+    plt.savefig('agroculture.png', bbox_inches="tight", dpi = 300)
+    plt.show()
+
+    
+line_agriculture(df_agriculture_tr)
 #Calling the bar_no2 function    
 bar_no2(df_no2)
     
